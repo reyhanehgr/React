7dce6f0861c2b641ea97636864b5898dd664368f
reyhaneh gerami <32893822+reyhanehgr@users.noreply.github.com>
2026-09-24 12:42:35 +0300

    Add files via upload
    A React form built with React Hook Form, Zod, and TypeScript. This exercise demonstrates schema-based validation, custom error messages, type inference with Zod, and form submission using zodResolver.

---
+++ Form Validation with React Hook Form and Zod/App.tsx
@@ -0,0 +1,13 @@
+
+import ValidationFormZod from "./Components/ValidationFormZod";
+
+/*  rafce  */
+
+function App() {
+  return (
+    <div>
+      <ValidationFormZod />
+    </div>
+  );
+}
+export default App;
